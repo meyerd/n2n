6056ba26f1812f4ab74d3109b966db004054ac2f
wintsco <wintsco@f-m.fm>
2011-08-11 22:13:40 +0200

    NIST concat KDF, SPI random number generator
    added the NIST concatenation key derivation function (used to generate
            session keys)
    a new function to get random numbers in secure memory

--- crypto.c
+++ crypto.c
@@ -8,7 +8,7 @@
 #define AEAD_KEY_SIZE 16  // TODO remove
 #define AEAD_IV_SIZE 12
 #define AEAD_TAG_SIZE 16
-#define DERIV_HMAC_SIZE 48
+#define DERIV_HASH_SIZE 48
 
 static int CRYPTO_INITIALIZED = 0;
 
@@ -27,6 +27,7 @@ int crypto_init(void)
      * available and also drops privileges where needed.
      */
     //TODO dynamic secure memory management
+    //TODO unset CRYPTO_INITIALIZED when memory allocation fails
     gc_err = gcry_control(GCRYCTL_INIT_SECMEM, 16384, 0);
     if (gc_err != GPG_ERR_NO_ERROR) {
         traceEvent(TRACE_ERROR, "gcrypt init error: init sec mem");
@@ -64,6 +65,71 @@ void crypto_deinit(void)
     gnutls_global_deinit();
 }
 
+/* NIST concatenation key derivation function as in NIST SP 800-56A */
+static int derive_key(const void **ss, size_t sslen, uint32_t *from_spi,
+        uint32_t *to_spi, const void **salt, size_t saltlen,
+        const void *masterkey, size_t masterkeylen, void **derived,
+        size_t derivedlen)
+{
+    if (!CRYPTO_INITIALIZED)
+        return 1;
+    /* salt and ss should have a length of at least the digest size */
+    if (saltlen < DERIV_HASH_SIZE || sslen < DERIV_HASH_SIZE)
+        return 2;
+    /* currently not supported, but could be done by completely implementing
+     * the kdf
+     */
+    if (derivedlen != AEAD_KEY_SIZE)
+        return 3;
+
+    uint8_t *input;  // the text input buffer to the hash function
+    void *keymat;
+    size_t ptr = 0;
+    uint32_t counter = 1;
+    size_t counterlen = sizeof(uint32_t);
+    char algoid[] = "N2N edge AES-GCM 256 session key";
+    int algoidlen = 32;
+    size_t spilen = sizeof(uint32_t);
+    int gt_err;
+
+    size_t inputlen = counterlen + sslen;
+    inputlen += algoidlen + (2 * spilen) + saltlen + masterkeylen;
+
+    input = gnutls_malloc(inputlen);
+
+    /* hash(counter || shared_secret || otherinfo) */
+    counter = htonl(counter);  // must be big endian
+    memcpy(input + ptr, &counter, counterlen);
+    ptr += counterlen;
+    memcpy(input + ptr, *ss, sslen);
+    ptr += sslen;
+
+    /* otherinfo = algoid || partyu || partyv || supppubinfo || supprivinfo */
+    memcpy(input + ptr, algoid, algoidlen);
+    ptr += algoidlen;
+    memcpy(input + ptr, from_spi, spilen);
+    ptr += spilen;
+    memcpy(input + ptr, to_spi, spilen);
+    ptr += spilen;
+    memcpy(input + ptr, *salt, saltlen);
+    ptr += saltlen;
+    //TODO should we really use the master key here?
+    memcpy(input + ptr, masterkey, masterkeylen);
+
+    keymat = gnutls_malloc(DERIV_HASH_SIZE);
+    gt_err = gnutls_hash_fast(GNUTLS_DIG_SHA384, input, inputlen, keymat);
+    if (gt_err != GNUTLS_E_SUCCESS) {
+        traceEvent(TRACE_ERROR, "gnutls error: key derivation");
+        return gt_err;
+    }
+    gnutls_free(input);
+
+    *derived = gnutls_malloc(DERIV_HASH_SIZE);
+    memcpy(*derived, keymat, AEAD_KEY_SIZE);
+    gnutls_free(keymat);
+    return 0;
+}
+
 /* derive a session key from the shared secret 'ss' as source_key and the salt
  * using a HMAC based key derivation function (RFC 5869):
  * HKDF(salt, source_key, infostring, outlen) = K1 | K2 | ... | Kt
@@ -71,21 +137,21 @@ void crypto_deinit(void)
  *     K1 = HMAC(prk, infostring | 0x01)
  *     K2 = HMAC(prk, K1 | infostring | 0x02)
  *     K3 = HMAC(prk, K2 | infostring | 0x03) ...
+ * This function could be used instead of the concatenation function. For the
+ * sake of Suite B compliance we will stick with concatenation for now.
  */
-static int derive_key(const void **salt, size_t saltlen, const void **ss,
-        size_t sslen, void **derived, size_t derivedlen)
+static int derive_key_rfc5869(const void **salt, size_t saltlen,
+        const void **ss, size_t sslen, void **derived, size_t derivedlen)
 {
     if (!CRYPTO_INITIALIZED)
         return 1;
     /* salt and ss should have a length of at least the digest size */
-    if (saltlen < DERIV_HMAC_SIZE || sslen < DERIV_HMAC_SIZE
-        )
+    if (saltlen < DERIV_HASH_SIZE || sslen < DERIV_HASH_SIZE)
         return 2;
     /* currently not supported, but could be done by completely implementing
      * rfc 5869
      */
-    if (derivedlen > DERIV_HMAC_SIZE
-        )
+    if (derivedlen > DERIV_HASH_SIZE)
         return 3;
 
     int gt_err;
@@ -97,8 +163,8 @@ static int derive_key(const void **salt, size_t saltlen, const void **ss,
 
     uint8_t *prk;
     uint8_t *okm;
-    prk = gnutls_malloc(DERIV_HMAC_SIZE); // pseudorandom key
-    okm = gnutls_malloc(DERIV_HMAC_SIZE); // output key material
+    prk = gnutls_malloc(DERIV_HASH_SIZE); // pseudorandom key
+    okm = gnutls_malloc(DERIV_HASH_SIZE); // output key material
     derived = gnutls_malloc(derivedlen);
 
     /* the salt is used as the key, the shared secret is used as data */
@@ -109,7 +175,7 @@ static int derive_key(const void **salt, size_t saltlen, const void **ss,
         return gt_err;
     }
 
-    gt_err = gnutls_hmac_fast(GNUTLS_MAC_SHA384, prk, DERIV_HMAC_SIZE, data,
+    gt_err = gnutls_hmac_fast(GNUTLS_MAC_SHA384, prk, DERIV_HASH_SIZE, data,
             datalen, okm);
     if (gt_err != GNUTLS_E_SUCCESS) {
         traceEvent(TRACE_ERROR, "gnutls error: key derivation");
@@ -119,6 +185,23 @@ static int derive_key(const void **salt, size_t saltlen, const void **ss,
     memcpy(*derived, okm, derivedlen);
     gnutls_free(prk);
     gnutls_free(okm);
+    return 0;
+}
+
+//TODO make sure this is dealloc'd correctly
+/* Generate a random number in secure memory. Use this for SPI generation */
+int crypto_rnd(void **rnd, size_t len)
+{
+    if (!CRYPTO_INITIALIZED)
+        return 1;
+    int gt_err;
+
+    *rnd = gnutls_malloc(len);
+    gt_err = gnutls_rnd(GNUTLS_RND_RANDOM, *rnd, len);
+    if (gt_err != GNUTLS_E_SUCCESS) {
+        traceEvent(TRACE_ERROR, "gnutls error: cipher init");
+        return gt_err;
+    }
     return 0;
 }
 
@@ -239,8 +322,7 @@ int aes_gcm_authdec(gnutls_cipher_hd_t ctx, uint8_t *in, size_t in_len,
     // authenticate with padding of wrong lenght, else timing attack
 
     /* abuse the pkcs #12 MAC error to signal failed authentication */
-    if (in_len < AEAD_IV_SIZE + AEAD_TAG_SIZE
-        )
+    if (in_len < AEAD_IV_SIZE + AEAD_TAG_SIZE)
         return GNUTLS_E_MAC_VERIFY_FAILED;
 
     /* set new IV */
--- crypto.h
+++ crypto.h
@@ -2,10 +2,15 @@
 
 int crypto_init(void);
 void crypto_deinit(void);
+int crypto_rnd(void **rnd, size_t len);
+
 int aes_gcm_session_create(gnutls_datum_t *key, gnutls_cipher_hd_t **ctx);
 void aes_gcm_session_destroy(gnutls_cipher_hd_t *ctx);
-void *aes_gcm_dummy_key(void);
+
 int aes_gcm_authenc(gnutls_cipher_hd_t ctx, uint8_t *pt, size_t pt_len,
         uint8_t *out, size_t out_len, uint8_t *ad, size_t ad_len);
 int aes_gcm_authdec(gnutls_cipher_hd_t ctx, uint8_t *in, size_t in_len,
         uint8_t *out, size_t out_len, uint8_t *ad, size_t ad_len);
+
+void *aes_gcm_dummy_key(void);
+
