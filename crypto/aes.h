9a8cab44d2481c857aa9dbbfec021d358d1e60b1
wintsco <wintsco@f-m.fm>
2011-10-15 11:17:57 +0200

    security associations modified

--- crypto/aes.h
+++ crypto/aes.h
@@ -1,14 +1,16 @@
-#define AEAD_KEY_SIZE 16  // TODO remove
 #define AEAD_IV_SIZE 12
 #define AEAD_TAG_SIZE 16
 
-int aes_gcm_session_create(gnutls_datum_t *key, gnutls_cipher_hd_t **ctx);
-void aes_gcm_session_destroy(gnutls_cipher_hd_t *ctx);
+/* security association: never ever manipulate this outside of aes.c */
+typedef struct {
+    uint32_t *salt;
+    uint64_t *counter;
+    gnutls_cipher_hd_t ctx;
+} sa_t;
 
-int aes_gcm_authenc(gnutls_cipher_hd_t ctx, uint8_t *pt, size_t pt_len,
-        uint8_t *out, size_t out_len, uint8_t *ad, size_t ad_len);
-int aes_gcm_authdec(gnutls_cipher_hd_t ctx, uint8_t *in, size_t in_len,
-        uint8_t *out, size_t out_len, uint8_t *ad, size_t ad_len);
-
-void *aes_gcm_dummy_key(void);
-
+int aes_sa_create(sa_t *sa, void *sk, int sklen);
+void aes_sa_destroy(sa_t *sa);
+int aes_authenc(sa_t *sa, void *pt, size_t pt_len, void *ad, size_t ad_len,
+        void *out, size_t out_len);
+int aes_authdec(sa_t *sa, void *in, size_t in_len, void *ad, size_t ad_len,
+        void *out, size_t out_len);
